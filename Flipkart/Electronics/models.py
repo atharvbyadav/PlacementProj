89f86bd4491b8ada04003e0fd5e381e000cf7afc
atharvbyadav <atharvbyadav@gmail.com>
2025-04-16 15:14:19 +0530

    Adding

--- Flipkart/Electronics/models.py
+++ Flipkart/Electronics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+# class Order(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField()
