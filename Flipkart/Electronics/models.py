37594942ff462793361aaae7c8a4d591c083f397
atharvbyadav <atharvbyadav@gmail.com>
2025-04-17 16:24:42 +0530

    Adding

--- Flipkart/Electronics/models.py
+++ Flipkart/Electronics/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
--- Flipkart/Electronics/views.py
+++ Flipkart/Electronics/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .forms import ProductForm
+
 # Create your views here.
 def home(request):
     products = Product.objects.all()
-    return render(request, 'Electronics/home.html', {'products': products})
+    fm = ProductForm()
+    return render(request, 'Electronics/home.html', {'products': products, 'fm': fm})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
